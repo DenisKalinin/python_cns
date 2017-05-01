cdb578473f78bb2df13041c2b08bc6110e8e30a9
DenisKalinin <mikadi@mail.ru>
2017-05-01 10:02:25 +0300

    homework 1.2

--- homework/1-5.py
+++ homework/1-5.py
@@ -2,13 +2,12 @@
 
 N = int(input())
 
-
 l1 = [x for x in range(0, N+1)]
 l1 = l1[0:int(N/2)]
 
 if N%2:
     l1 += reversed(l1)
-    l1.insert(int(N/2), 1)
+    l1.insert(int(N/2), int(N/2))
 else:
     l1 += reversed(l1)
 
