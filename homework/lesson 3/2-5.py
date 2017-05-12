78f5d1e5eb8d710772f0413a422946457431a6f5
Денис Калинин <DMKalinin@sberned.lc>
2017-05-12 11:21:30 +0300

    homework 2.2

--- homework/lesson 3/2-5.py
+++ homework/lesson 3/2-5.py
@@ -1,18 +1,18 @@
 #5. Написать функцию, которой можно передавать аргументы либо списком/кортежем, либо по одному.
 # Функция производит суммирование всех аргументов
-n=0
-def amount(i):
-    global n
-    if type(i) == int:
-        n += i
-        return n
-    else:
-        return sum(i)
 
-print(amount((7, 7, 7)))
+def amount(*args):
+    n = 0
+    for i in args:
+        if type(i) == int:
+            n = sum(args)
+        else:
+            n+=sum(i)
+    return n
+
 print(amount([1, 3, 5, 6]))
-print(amount(5))
-print(amount([3, 5]))
-print(amount(5))
-print(amount((7, 4)))
-print(amount(3))
+print(amount(1, 2, 4, 4, 5))
+print(amount((1, 3, 4, 1)))
+print(amount([3,]))
+print(amount(2))
+print(amount((1,)))
